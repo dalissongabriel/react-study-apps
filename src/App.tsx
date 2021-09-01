8053334d8b039fbfc6af2f75ecea186596cc1257
Alisson Gabriel <alisson.gabriel@assertivasolucoes.com.br>
2021-09-01 16:00:21 -0300

    feat: migrate basestep to use material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+
 import { BaseStep } from './PizzaOrder/BaseStep';
 import { CheeseStep } from './PizzaOrder/CheeseStep';
 import { CrustStep } from './PizzaOrder/CrustStep';
 import { SauceStep } from './PizzaOrder/SauceStep';
-import { Provider } from "react-redux";
-import { store }from "./app/store";
 import { Result } from "./PizzaOrder/Result";
 
 export const App: React.FC = () => {
     return (
-    <Provider store={store}>
-        <Router>
-            <Route exact path="/">
-                <Redirect to="/pizza-order/base-step"/>
-            </Route>
-            <Route path="/pizza-order/base-step" component={BaseStep}/>
-            <Route path="/pizza-order/crust-step" component={CrustStep}/>
-            <Route path="/pizza-order/cheese-step" component={CheeseStep}/>
-            <Route path="/pizza-order/sauce-step" component={SauceStep}/>
-            <Route path="/pizza-order/result" component={Result}/>
-        </Router>
-    </Provider>
+        <Provider store={store}>
+            <Router>
+                <Route exact path="/">
+                    <Redirect to="/pizza-order/base-step" />
+                </Route>
+                <Route path="/pizza-order/base-step" component={BaseStep} />
+                <Route path="/pizza-order/crust-step" component={CrustStep} />
+                <Route path="/pizza-order/cheese-step" component={CheeseStep} />
+                <Route path="/pizza-order/sauce-step" component={SauceStep} />
+                <Route path="/pizza-order/result" component={Result} />
+            </Router>
+        </Provider>
     )
 }
--- src/PizzaOrder/BaseStep.tsx
+++ src/PizzaOrder/BaseStep.tsx
@@ -1,30 +1,73 @@
-import { useAppDispatch } from '../app/hooks'; 
+import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { useHistory } from 'react-router';
 import { useForm } from 'react-hook-form';
+import {
+  Button,
+  Container,
+  createStyles,
+  Theme,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Select,
+  FormControl
+} from '@material-ui/core';
 
-import { chooseBase } from './pizzaOrderSlice';
+import { chooseBase, selectPizzaOrder } from './pizzaOrderSlice';
+import { Header } from '../shared/Header';
+
 import { PizzaOrder } from './types';
 
-export const BaseStep: React.FC = () => {
-    const dispatch = useAppDispatch()
-    const history = useHistory();
-    const { register, handleSubmit } = useForm();
 
-    const onSubmit = ( { base }: PizzaOrder) => {
-        dispatch(chooseBase(base));
-        history.push("/pizza-order/crust-step")
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    flex: {
+      width: '100%'
+    },
+    button: {
+      marginTop: theme.spacing(3),
     }
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor="base">Selecione a base:</label>
-                <select id="base" {...register("base")}>
-                    <option value="small">Pequena</option>
-                    <option value="medium">Média</option>
-                    <option value="large">Família</option>
-                </select>
-            </div>
-            <button>Próxima etapa</button>
-        </form>
-    )
+  }),
+);
+
+
+export const BaseStep: React.FC = () => {
+  const dispatch = useAppDispatch()
+  const history = useHistory();
+  const { register, handleSubmit } = useForm();
+  const { base } = useAppSelector(selectPizzaOrder)
+  const classes = useStyles();
+
+  const onSubmit = ({ base }: PizzaOrder) => {
+    dispatch(chooseBase(base));
+    history.push("/pizza-order/crust-step")
+  }
+  return (
+    <Container maxWidth="sm">
+      <Header>Pedido de Pizza</Header>
+      <form className={classes.flex} noValidate onSubmit={handleSubmit(onSubmit)}>
+        <FormControl fullWidth>
+          <InputLabel id="base-label">Selecione o tamanho:</InputLabel>
+          <Select
+            fullWidth
+            labelId="base-label"
+            id="base"
+            defaultValue={base}
+            {...register("base")}
+          >
+            <MenuItem selected value={"small"}>Pequena</MenuItem>
+            <MenuItem value={"medium"}>Média</MenuItem>
+            <MenuItem value={"large"}>Família</MenuItem>
+          </Select>
+        </FormControl>
+        <Button
+          type="submit"
+          className={classes.button}
+          variant="contained"
+          color="primary">Próxima
+          etapa
+        </Button>
+      </form>
+    </Container>
+  )
 }
